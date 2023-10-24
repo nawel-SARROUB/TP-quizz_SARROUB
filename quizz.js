be9b19a36402e0474a5143e10874ec1a112bb36f
nawel_MAISON <nawel.sarroub@gmail.com>
2023-10-24 14:07:11 +0200

    first commit

---
+++ quizz.js
@@ -0,0 +1,12 @@
+let paris = document.getElementById("paris");
+let lyon = document.getElementById("lyon");
+let mars = document.getElementById("mars");
+
+let pied = document.getElementById("pied");
+let ventre = document.getElementById("ventre");
+let nez = document.getElementById("nez");
+
+let gutenTag = document.getElementById("gutenTag");
+let kalimera = document.getElementById("kalimera");
+let buongiorno = document.getElementById("buongiorno");
+
